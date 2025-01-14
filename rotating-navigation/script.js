8d2949bd5138102523fd67fe67c9509b1a8c2de7
tdang64 <tdang64@my.centennialcollege.ca>
2025-01-13 22:12:50 -0500

    first

---
+++ rotating-navigation/script.js
@@ -0,0 +1,6 @@
+const open = document.getElementById("open");
+const close = document.getElementById("close");
+const container = document.querySelector(".container");
+
+open.addEventListener("click", () => container.classList.add("is-open"));
+close.addEventListener("click", () => container.classList.remove("is-open"));
